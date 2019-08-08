03f869df894b37f131abbd6c07615ded4e0ca65f
Muhammad Rehan Baig <mrb5only@gmail.com>
2019-08-08 13:25:00 +0500

    Basic initial commit

---
+++ Documents/Visual Studio 2017/Projects/REstate/REstate/REstate/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace REstate
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void p1Clicked(object sender, EventArgs e)
+        {
+            UrlBrowse.Source="http://www.google.com?id=1";
+        }
+        void p2Clicked(object sender, EventArgs e)
+        {
+            UrlBrowse.Source = "http://m.facebook.com";
+        }
+        void p3Clicked(object sender, EventArgs e)
+        {
+            UrlBrowse.Source = "http://www.twitter.com";
+        }
+        void p4Clicked(object sender, EventArgs e)
+        {
+            UrlBrowse.Source = "http://www.linkedin.com";
+        }
+    }
+}
